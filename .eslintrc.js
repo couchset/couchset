0803bd52d622458bb608fdc6dccf619add2a41bd
cmuhoza <ceddymuhoza@gmail.com>
2021-09-21 03:44:01 -0400

    fix all

--- .eslintrc.js
+++ .eslintrc.js
@@ -36,7 +36,8 @@ module.exports = {
         "simple-import-sort/imports": "error",
         "simple-import-sort/exports": "error",
         "import/first": "error",
-        "import/newline-after-import": "error"
+        "import/newline-after-import": "error",
+        "import/no-duplicates": "error"
 
     },
     settings: {},
